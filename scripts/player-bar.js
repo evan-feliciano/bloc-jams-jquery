de876020a0b00912050be2d8b711ad904c7324c7
evan-feliciano <56705326+evan-feliciano@users.noreply.github.com>
2020-03-24 13:47:16 -0400

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,28 +1,27 @@
-{
-  $('button#play-pause').on('click', function() {
-    player.playPause();
-    $(this).attr('playState', player.playState);
-  });
+$('button#play-pause').on('click', function() {
+  player.playPause();
+  $(this).attr('playState', player.playState);
+});
 
-  $('button#next').on('click', function() {
-    if (player.playState !== 'playing') { return; }
+$('button#next').on('click', function() {
+  if (player.playState !== 'playing') { return; }
 
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
+  const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+  const nextSongIndex = currentSongIndex + 1;
+  if (nextSongIndex >= album.songs.length) { return; }
 
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+  const nextSong = album.songs[nextSongIndex];
+  player.playPause(nextSong);
 
-  });
+});
 
-  $('button#previous').on('click', function() {
+$('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
     if (previousSongIndex >= album.songs[0]) { return; }
 
-    const nextSong = album.songs[previousSongIndex];
+    const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
-)};
+});
