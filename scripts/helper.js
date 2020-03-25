7cae410737c0f505a63adf1af77352ab0d3f6c7c
evan-feliciano <56705326+evan-feliciano@users.noreply.github.com>
2020-03-25 10:52:31 -0400

    Update helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,6 +1,9 @@
 class Helper {
   playPauseAndUpdate(song) {
+    player. playPause(song);
+    $('#time-control .duration').text( duration );
     helper.playPauseAndUpdate  = $(player.playPause).val()
+
   }
 };
 
