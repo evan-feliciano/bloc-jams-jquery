c6f3e9dfce552da3a7d5c43bd68edaceef580fab
evan-feliciano <56705326+evan-feliciano@users.noreply.github.com>
2020-03-25 13:02:38 -0400

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -37,7 +37,7 @@ $('button#previous').on('click', function() {
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex < album.songs[0]) { return; }
+    if (previousSongIndex < 0) { return; }
 
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
